d591fa5a91eb6281b6e258f0759917252c898e25
Ruskan617 <zinnatullinrusan280@gmail.com>
2025-01-18 21:43:11 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+user_name = "Рушан"
+title = " Природа"
+content = " Изучение поведения бабочек"
+status = " Подготовка"
+created_date = " 16.01.2025"
+issue_date = " 30.01.2025"
+
+print(user_name)
+print(title)
+print(content)
+print(status)
+print(created_date[0:6])
+print(issue_date[0:6])

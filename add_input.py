f2823cf9a6bb96a23f730d37b322a194712c59ee
Ruskan617 <zinnatullinrusan280@gmail.com>
2025-01-19 08:01:58 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,7 @@
+user_name = input("Введите имя")
+title = input("Заголовок")
+content = input("Тема")
+status = input("Статус")
+created_date = int(input("Начало проекта, напр: 01.01.2025"))
+issue_date = int(input("Завершение проекта, напр: 01.02.2025"))
+

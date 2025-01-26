72f646e59d3491940e306f4edd04f35d4d578dfe
Ruskan617 <zinnatullinrusan280@gmail.com>
2025-01-26 08:07:22 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,15 @@
+user_name = "Введите имя"
+title = "Заголовок"
+title1 = " Заголовок 2"
+title2 = " Заголовок3"
+tittles = [title, title1, title2]
+content = "Тема"
+status = "Статус"
+created_date = "Начало проекта, напр: 01.01.2025"
+issue_date = "Завершение проекта, напр: 01.02.2025"
+note = [ input(user_name),
+         input(tittles),
+          input(content),
+          input(status),
+          input(created_date),
+        input(issue_date)]
